2b79940c674abc0e33e6ec583f2ae239d3212b2b
Ferid HELALI <helaliferid@gmail.com>
2022-03-06 21:34:31 +0100

    fix API_URI form movies service

--- src/app/movies/movie.service.ts
+++ src/app/movies/movie.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { IMovie } from './movie.model';
 
 
@@ -11,19 +12,19 @@ export class MovieService {
   constructor(private http:HttpClient){}
 
   getAllMovies():Observable<any>{
-     return this.http.get('http://localhost:3000/movies/featured') as Observable<IMovie[]>
+     return this.http.get(`${environment.BASE_API_URI}/movies/featured`) as Observable<IMovie[]>
   }
 
   addMovie(movie:IMovie):Observable<IMovie>{
-      return this.http.post('http://localhost:3000/movies',movie) as Observable<IMovie>
+      return this.http.post(`${environment.BASE_API_URI}/movies`,movie) as Observable<IMovie>
   }
 
 
   removeMovie(movie:IMovie): Observable<any>{
-     return this.http.delete(`http://localhost:3000/movies/${movie.id}`)
+     return this.http.delete(`${environment.BASE_API_URI}/movies/${movie.id}`)
   }
 
   updateMovie(movie:IMovie):Observable<any>{
-    return this.http.put(`http://localhost:3000/movies/${movie.id}`,movie)
+    return this.http.put(`${environment.BASE_API_URI}/movies/${movie.id}`,movie)
   }
 }
